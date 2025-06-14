a10c71019faab9e83f91f4435f74e313138612da
DM 007 <210326445+DM-WEB-007@users.noreply.github.com>
2025-06-14 16:13:37 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/DM-WEB-007/',
   plugins: [
+    
     tailwindcss(),
+    
     react()],
 })
